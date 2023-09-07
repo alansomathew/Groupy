0b674fdf5ce9b627af197ca0ba9628cc91a2dff0
alansomathew <alansomathew10@gmail.com>
2023-09-07 20:49:10 +0530

    changes

--- Organizer/models.py
+++ Organizer/models.py
@@ -11,3 +11,21 @@ class Room(models.Model):
     events=models.ForeignKey(Event,on_delete=models.CASCADE)
     number=models.CharField(max_length=50)
     capacity=models.IntegerField(default=0)
+
+
+class PrivateEvent(models.Model):
+    code=models.CharField(max_length=50,unique=True)
+    rooms=models.IntegerField()
+    number=models.IntegerField()
+    org=models.ForeignKey("Guest.organiser",on_delete=models.CASCADE)
+    status=models.IntegerField(default=0)
+
+class PrivateCodes(models.Model):
+    event=models.ForeignKey(PrivateEvent,on_delete=models.CASCADE)
+    code=models.CharField(max_length=50,unique=True)
+    status=models.IntegerField(default=0)
+
+class PrivateRoom(models.Model):
+    events=models.ForeignKey(PrivateEvent,on_delete=models.CASCADE)
+    number=models.CharField(max_length=50)
+    capacity=models.IntegerField(default=0)
